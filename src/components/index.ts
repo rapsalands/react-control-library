867a11e310c26026a0c9a83422733b5bc2666744
Rapsy Tree <rapsalands@gmail.com>
2020-11-06 00:44:49 -0800

    Publish settings.

---
+++ src/components/index.ts
@@ -0,0 +1,19 @@
+import AlphaNumeric from "./form/controls/alphaNumeric";
+import DecimalInput from "./form/controls/decimalInput";
+import Email from "./form/controls/email";
+import MaskedInput from "./form/controls/maskedInput";
+import NumberInput from "./form/controls/numberInput";
+import Password from "./form/controls/password";
+import SecureMaskedInput from "./form/controls/secureMaskedInput";
+import TextInput from "./form/controls/textInput";
+
+export {
+    NumberInput,
+    DecimalInput,
+    Email,
+    Password,
+    AlphaNumeric,
+    TextInput,
+    MaskedInput,
+    SecureMaskedInput
+};
