310b16411221c6fc53d63a5f81193ba2c4156390
Rapsy Tree <rapsalands@gmail.com>
2020-11-18 01:44:02 -0800

    Added SO link.

--- src/components/form/formValidations.ts
+++ src/components/form/formValidations.ts
@@ -94,36 +94,38 @@ export function validateForRestriction(detail: IDetail | null, value: any, props
     return new DetailIns(detail, value, true, null);
 }
 
-function scorePassword(password): number {
-    let score = 0;
-    if (!password)
-        return score;
-
-    // award every unique letter until 5 repetitions
-    const letters = new Object();
-    for (var i = 0; i < password.length; i++) {
-        letters[password[i]] = (letters[password[i]] || 0) + 1;
-        score += 5.0 / letters[password[i]];
-    }
-
-    // bonus points for mixing it up
-    const variations = {
-        digits: /\d/.test(password),
-        lower: /[a-z]/.test(password),
-        upper: /[A-Z]/.test(password),
-        nonWords: /\W/.test(password),
-    }
-
-    let variationCount = 0;
-    for (let check in variations) {
-        variationCount += (variations[check] == true) ? 1 : 0;
-    }
-    score += (variationCount - 1) * 10;
-
-    return score;
-}
-
+// Referred from: https://stackoverflow.com/a/11268104/815600
 function checkPassStrength(password): { score: number, strength: PasswordStrength } {
+
+    function scorePassword(password): number {
+        let score = 0;
+        if (!password)
+            return score;
+
+        // award every unique letter until 5 repetitions
+        const letters = new Object();
+        for (var i = 0; i < password.length; i++) {
+            letters[password[i]] = (letters[password[i]] || 0) + 1;
+            score += 5.0 / letters[password[i]];
+        }
+
+        // bonus points for mixing it up
+        const variations = {
+            digits: /\d/.test(password),
+            lower: /[a-z]/.test(password),
+            upper: /[A-Z]/.test(password),
+            nonWords: /\W/.test(password),
+        }
+
+        let variationCount = 0;
+        for (let check in variations) {
+            variationCount += (variations[check] == true) ? 1 : 0;
+        }
+        score += (variationCount - 1) * 10;
+
+        return score;
+    }
+
     const score = scorePassword(password);
     let strength = PasswordStrength.VeryWeak;
     if (score > 90)
