1e085fe8a5949d39db4cf3325490a67fafe31812
Rapsy Tree <rapsalands@gmail.com>
2020-12-02 00:41:38 -0800

    Partial paste working now.

--- src/components/form/controls/alphaNumeric.tsx
+++ src/components/form/controls/alphaNumeric.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { IAllowSymbolsProps } from '../../shared/interfacesDelegates/controlnterfaces';
 import { ValidationIns } from '../formPropsIns';
+import FormRegex from '../formRegex';
+import formUtility from '../formUtility';
 import regexVali from '../validation/regexVali';
 import CustomInput from './customInput';
 
@@ -8,8 +10,16 @@ const AlphaNumeric: React.FC<IAllowSymbolsProps> = ({ allowSymbols = '', ...prop
 
     const validation = new ValidationIns((data) => regexVali.alphaNumeric(data, allowSymbols));
 
+    function extractToSet(data): string {
+        return formUtility.getEachValidCharacter(data, (d) => {
+            const anValid = regexVali.regexValidate(FormRegex.alphaNumeric(), d);
+            const asValid = regexVali.regexValidate(FormRegex.custom(allowSymbols), d);
+            return anValid || asValid;
+        });
+    }
+
     return (
-        <CustomInput validation={validation} {...props} />
+        <CustomInput validation={validation} {...props} extractValueToSet={extractToSet} />
     );
 };
 
--- src/components/form/controls/decimalInput.tsx
+++ src/components/form/controls/decimalInput.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import AppSettings from '../../shared/appSettings';
 import { IDecimalInputProps } from '../../shared/interfacesDelegates/controlnterfaces';
 import { ValidationIns } from '../formPropsIns';
+import FormRegex from '../formRegex';
+import formUtility from '../formUtility';
 import regexVali from '../validation/regexVali';
 import CustomInput from './customInput';
 
@@ -11,8 +13,14 @@ const DecimalInput: React.FC<IDecimalInputProps> = ({ decimalLimit = 2, ...props
         return regexVali.decimal(data, decimalLimit, props.maxLength || AppSettings.decimal.maxLength)
     });
 
+    function extractToSet(data): string {
+        return formUtility.getEachValidCharacter(data, (d) => {
+            return regexVali.regexValidate(FormRegex.decimal(), d);
+        });
+    }
+
     return (
-        <CustomInput validation={validation} {...props} />
+        <CustomInput validation={validation} {...props} extractValueToSet={extractToSet} />
     );
 };
 
