3da3a1ee74af4b3a0ddb39f43ee8f6e3e50872a7
Rapsy Tree <rapsalands@gmail.com>
2020-12-21 02:08:05 -0800

    Exporting Checkbox & Radiobutton.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,11 @@
 import AlphaNumeric from "./form/controls/alphaNumeric";
+import Checkbox from "./form/controls/checkbox";
 import DecimalInput from "./form/controls/decimalInput";
 import Email from "./form/controls/email";
 import MaskedInput from "./form/controls/maskedInput";
 import NumberInput from "./form/controls/numberInput";
 import Password from "./form/controls/password";
+import RadioButton from "./form/controls/radioButton";
 import SecureInput from "./form/controls/secureInput";
 import TextInput from "./form/controls/textInput";
 
@@ -15,5 +17,7 @@ export {
     AlphaNumeric,
     TextInput,
     MaskedInput,
-    SecureInput as SecureMaskedInput
+    SecureInput as SecureMaskedInput,
+    Checkbox,
+    RadioButton
 };
